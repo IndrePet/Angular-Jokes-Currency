d8526c69dab5a4c61dac03b05893a1c578d93b5f
Indrė <90854409+IndrePet@users.noreply.github.com>
2022-06-09 19:55:55 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { JokeComponent } from './components/joke/joke.component';
+import { CurrencyConverterComponent } from './components/currency-converter/currency-converter.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, JokeComponent, CurrencyConverterComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models/joke.ts
@@ -0,0 +1,7 @@
+export interface Joke {
+  joke: string;
+  category: string;
+  id: number;
+  type: string;
+  error: boolean;
+}
